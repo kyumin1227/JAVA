63ec3dfaebd3aa2e21d3f563c0fb4938f3fca74f
Kyumin <kyumin12271227@gmail.com>
2023-03-04 17:36:21 +0900

    Java 203p 204p commit

---
+++ TelevisionTest.java
@@ -0,0 +1,16 @@
+public class TelevisionTest {
+    public static void main(String[] args) {
+        Television myTv = new Television();
+        myTv.channel = 6;
+        myTv.volume = 9;
+        myTv.onOff = true;
+        myTv.print();
+
+        Television myTv2 = new Television();
+        myTv2.channel = 2;
+        myTv2.volume = 13;
+        myTv2.onOff = false;
+        myTv2.print();
+        
+    }
+}
