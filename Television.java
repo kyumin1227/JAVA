7548b9cd54a40244d8f078091d8998ed4ec4c706
Kyumin <kyumin12271227@gmail.com>
2023-03-05 02:08:57 +0900

    203p

--- Television.java
+++ Television.java
@@ -2,7 +2,13 @@ public class Television {
     int channel;
     int volume;
     boolean onOff;
-    void print() {
-        System.out.println("채널은 "+channel+"이고 볼륨은 "+volume+"입니다.");
+    
+    public static void main(String[] args) {
+        Television tv = new Television();
+        tv.channel = 7;
+        tv.volume = 5;
+        tv.onOff = true;
+
+        System.out.println("채널은 "+tv.channel+"이고 볼륨은 "+tv.volume+"입니다.");
     }
 }
