cb77503dacc4bc57e014f3ee91fc2c92b9e567f0
Kyumin <kyumin12271227@gmail.com>
2023-03-05 02:26:44 +0900

    209p

---
+++ Box.java
@@ -0,0 +1,13 @@
+public class Box {
+    int width;
+    int length;
+    int height;
+
+    public static void main(String[] args) {
+        Box firstBox = new Box();
+        firstBox.height = 30;
+        firstBox.length = 20;
+        firstBox.width = 20;
+        System.out.println("상자의 가로, 세로, 높이는 "+firstBox.length+", "+firstBox.width+" ,"+firstBox.height+"입니다.");
+    }
+}
