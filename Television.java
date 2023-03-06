0fc866f16e7f90cc561a0a15d844677569b6628e
Kyumin <kyumin12271227@gmail.com>
2023-03-06 19:15:13 +0900

    P215

--- Television.java
+++ Television.java
@@ -6,6 +6,9 @@ public class Television {
     void print() {
         System.out.println("채널은 "+channel+"이고 볼륨은 "+volume+"입니다.");
     }
+    int getChannel() {
+        return channel;
+    }
     
     public static void main(String[] args) {
         Television myTv = new Television();
@@ -13,11 +16,15 @@ public class Television {
         myTv.volume = 5;
         myTv.onOff = true;
         myTv.print();
+        int myCh = myTv.getChannel();
+        System.out.println("현재 채널은 " +myCh+"입니다.");
 
         Television yourTv = new Television();
         yourTv.channel = 13;
         yourTv.volume = 20;
         yourTv.onOff = false;
         yourTv.print();
+        int yourCh = yourTv.getChannel();
+        System.out.println("현재 채널은 " +yourCh+"입니다.");
     }
 }
