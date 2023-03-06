97ff2a9b9dbb5788b46665eaa2e3521d2e78d8af
Kyumin <kyumin12271227@gmail.com>
2023-03-06 19:27:31 +0900

    P218

--- Television.java
+++ Television.java
@@ -9,22 +9,28 @@ public class Television {
     int getChannel() {
         return channel;
     }
+    void setChannel(int ch) {
+        channel = ch;
+    }
     
     public static void main(String[] args) {
         Television myTv = new Television();
-        myTv.channel = 7;
-        myTv.volume = 5;
-        myTv.onOff = true;
-        myTv.print();
-        int myCh = myTv.getChannel();
-        System.out.println("현재 채널은 " +myCh+"입니다.");
+        myTv.setChannel(27);
+        int ch = myTv.getChannel();
+        System.out.println("채널이 "+ch+ "번으로 변경되었습니다.");
+        // myTv.channel = 7;
+        // myTv.volume = 5;
+        // myTv.onOff = true;
+        // myTv.print();
+        // int myCh = myTv.getChannel();
+        // System.out.println("현재 채널은 " +myCh+"입니다.");
 
-        Television yourTv = new Television();
-        yourTv.channel = 13;
-        yourTv.volume = 20;
-        yourTv.onOff = false;
-        yourTv.print();
-        int yourCh = yourTv.getChannel();
-        System.out.println("현재 채널은 " +yourCh+"입니다.");
+        // Television yourTv = new Television();
+        // yourTv.channel = 13;
+        // yourTv.volume = 20;
+        // yourTv.onOff = false;
+        // yourTv.print();
+        // int yourCh = yourTv.getChannel();
+        // System.out.println("현재 채널은 " +yourCh+"입니다.");
     }
 }
