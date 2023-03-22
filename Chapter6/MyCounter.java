eeb74fd04bb2bd1147a2b3b87da993516afdf935
Kyumin <kyumin12271227@gmail.com>
2023-03-22 15:07:36 +0900

    275P

--- Chapter6/MyCounter.java
+++ Chapter6/MyCounter.java
@@ -1,8 +1,8 @@
 package Chapter6;
 
 public class MyCounter {
-    int value;
-    void inc(int a) {
-        a = a+1;
+    int value = 0;
+    void inc(MyCounter ctr) {
+        ctr.value = ctr.value + 1;
     }
 }
--- Chapter6/MyCounterTest1.java
+++ Chapter6/MyCounterTest1.java
@@ -3,9 +3,9 @@ package Chapter6;
 public class MyCounterTest1 {
     public static void main(String[] args) {
         MyCounter obj = new MyCounter();
-        int x = 10;
-
-        obj.inc(x);
-        System.out.println("x = "+x);
+        
+        System.out.println("obj.value = "+obj.value);
+        obj.inc(obj);
+        System.out.println("obj.value = "+obj.value);
     }
 }
