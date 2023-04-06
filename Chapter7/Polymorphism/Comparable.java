e8828d344550929d7d4a477e141c0ab82b9e20c1
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-04-06 19:28:40 +0900

    수업

---
+++ Chapter7/Polymorphism/Comparable.java
@@ -0,0 +1,6 @@
+package Chapter7.Polymorphism;
+
+public abstract class Comparable {
+    // 내가 더 크면 1, anotherVal하고 값이 같으면 0, 내가 더 작으면 -1
+    public abstract int compareTo(Comparable anotherVal);
+}
--- Chapter7/Polymorphism/Util.java
+++ Chapter7/Polymorphism/Util.java
@@ -1,10 +1,10 @@
 package Chapter7.Polymorphism;
 
 public class Util {
-    public static void main(int[] values) {
-        for(int i = 0; i < values.length; i++) {
+    public static void selectionSort(int[] values) {
+        for (int i = 0; i < values.length; i++) {
             int minIdx = i;
-            for(int j = i+1; j < values.length; j++) {
+            for (int j = i + 1; j < values.length; j++) {
                 // 현재까지 찾은 최소값(values[minIdx])과
                 // value[j]를 비교해서 values[j]가 더 작으면
                 // minIdx를 j로 설정 : minIdx = j
@@ -12,6 +12,36 @@ public class Util {
                     minIdx = j;
                 }
             }
+            // minIdx의 값과 i의 값을 교환
+            swap(i, minIdx, values);
         }
+    }
+
+    public static void selectionSort(Comparable[] values) {
+        for (int i = 0; i < values.length; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < values.length; j++) {
+                // 현재까지 찾은 최소값(values[minIdx])과
+                // value[j]를 비교해서 values[j]가 더 작으면
+                // minIdx를 j로 설정 : minIdx = j
+                if (values[minIdx].compareTo(values[j]) > 0) {
+                    minIdx = j;
+                }
+            }
+            // minIdx의 값과 i의 값을 교환
+            swap(i, minIdx, values);
+        }
+    }
+
+    private static void swap(int idx1, int idx2, Comparable[] values) {
+        Comparable tmp = values[idx1];
+        values[idx1] = values[idx2];
+        values[idx2] = tmp;
+    }
+
+    private static void swap(int idx1, int idx2, int[] values) {
+        int tmp = values[idx1];
+        values[idx1] = values[idx2];
+        values[idx2] = tmp;
     }
 }
